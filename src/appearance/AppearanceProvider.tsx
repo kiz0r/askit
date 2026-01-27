254de57d5411bcb45b3fdd230b21e531327ecac7
Kyrylo Khomenko <kkhomenko10@gmail.com>
2026-01-27 20:40:44 +0100

    feat(appearance): add tooling and component to provide and manage application appearance mode

---
+++ src/appearance/AppearanceProvider.tsx
@@ -0,0 +1,26 @@
+import { Theme } from '@radix-ui/themes';
+import React from 'react';
+import styles from './AppearanceProvider.module.scss';
+import { useAppearance } from './useAppearance';
+
+type Props = {
+  readonly children: React.ReactNode;
+};
+
+export const AppearanceProvider = React.memo((props: Props) => {
+  const appearance = useAppearance();
+
+  return (
+    <Theme
+      appearance={appearance.style}
+      className={styles.AppearanceProvider}
+      accentColor='iris'
+      grayColor='auto'
+      radius='medium'
+      panelBackground='translucent'
+      scaling='100%'
+    >
+      {props.children}
+    </Theme>
+  );
+});
