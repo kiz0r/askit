cec6210a1d375fcee750062bdf9a35167bffec33
Kyrylo Khomenko <kkhomenko10@gmail.com>
2026-01-29 21:17:09 +0100

    refactor(notifications)!: rework Notifications logic

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,6 +3,7 @@ import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 import { Provider as StoreProvider } from 'jotai';
 import { AppearanceProvider } from '../appearance/AppearanceProvider';
 import { ErrorBoundary } from '../error-boundary/ErrorBoundary';
+import { NotificationCenter } from '../notifications/NotificationCenter';
 import { QueryClientProvider } from '../providers/QueryClientProvider';
 import { isDev } from '../settings';
 import { store } from '../store';
@@ -16,7 +17,7 @@ export const Route = createRootRoute({
           <QueryClientProvider>
             <Outlet />
           </QueryClientProvider>
-          <Toaster />
+          <NotificationCenter />
         </ErrorBoundary>
         {isDev ? <TanStackRouterDevtools position='bottom-right' /> : null}
       </AppearanceProvider>
